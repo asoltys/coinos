9906a3f38e4d322d37130da415d297974eb0193f
Adam Soltys <asoltys@gmail.com>
2020-04-22 21:16:48 -0700

    lightning tor node

--- src/components/LightningNode.vue
+++ src/components/LightningNode.vue
@@ -2,13 +2,48 @@
   <v-card class="mb-2">
     <v-card-title>Lightning Node</v-card-title>
     <v-card-text>
-      <canvas id="qr" class="d-block mx-auto mb-4" @click="fullscreen" />
+      <canvas
+        v-if="qrText"
+        id="qr"
+        class="d-block mx-auto mb-4"
+        @click="fullscreen"
+      />
       <div class="text-center my-4">
-        <code class="black--text" :data-clipboard-text="node">{{ node }}</code>
+        <v-textarea
+          label="Clearnet"
+          v-model="clearnet"
+          readonly
+          auto-grow
+          class="body-2"
+          rows="1"
+        >
+          <template v-slot:append>
+            <v-btn icon @click="() => qr(clearnet)" class="ml-1" text>
+              <qrcode />
+            </v-btn>
+            <v-btn icon @click="() => copy(clearnet)" class="ml-1" text>
+              <v-icon>content_copy</v-icon>
+            </v-btn>
+          </template>
+        </v-textarea>
+        <v-textarea
+          label="Darknet"
+          v-model="darknet"
+          readonly
+          auto-grow
+          class="body-2"
+          rows="1"
+        >
+          <template v-slot:append>
+            <v-btn icon @click="() => qr(darknet)" class="ml-1" text>
+              <qrcode />
+            </v-btn>
+            <v-btn icon @click="() => copy(darknet)" class="ml-1" text>
+              <v-icon>content_copy</v-icon>
+            </v-btn>
+          </template>
+        </v-textarea>
       </div>
-      <v-btn @click="() => copy(node)" class="d-block mx-auto">
-        <v-icon class="mr-1">content_copy</v-icon><span>copy</span>
-      </v-btn>
     </v-card-text>
   </v-card>
 </template>
@@ -17,21 +52,33 @@
 import qr from 'qrcode';
 import Copy from '../mixins/Copy';
 import FullScreen from '../mixins/FullScreen';
+import Qrcode from 'vue-material-design-icons/Qrcode';
 
 export default {
+  components: { Qrcode },
   mixins: [Copy, FullScreen],
   data() {
     return {
-      node:
+      qrText: '',
+      darknet:
+        '2868e12f320073cad0c2959c42559fbcfd1aa326fcb943492ed7f02c9820aa399@jbx2afvrkuxopekkvipjcult26ffvu3t4lq5x7k4zcs3z7hovu4kdtyd.onion:9735',
+      clearnet:
         '02868e12f320073cad0c2959c42559fbcfd1aa326fcb943492ed7f02c9820aa399@coinos.io:9735',
     };
   },
-  mounted() {
-    let canvas = document.getElementById('qr');
-    if (!canvas) return;
-    qr.toCanvas(canvas, this.node, e => {
-      if (e) console.log(e);
-    });
+  methods: {
+    qr(text) {
+      if (text === this.qrText) this.qrText = '';
+      else this.qrText = text;
+
+      this.$nextTick(() => {
+        let canvas = document.getElementById('qr');
+        if (!canvas) return;
+        qr.toCanvas(canvas, text, e => {
+          if (e) console.log(e);
+        });
+      });
+    },
   },
 };
 </script>
--- src/components/Tor.vue
+++ src/components/Tor.vue
@@ -1,9 +1,9 @@
 
 <template>
   <v-card class="mb-2">
-    <v-card-title>Tor</v-card-title>
+    <v-card-title>Tor URL</v-card-title>
     <v-card-text class="white--text body-1">
-      Coinos is available as a tor hidden service: <a href="http://uzmim34vid5dkyzjxpel6zbkfz4qeqaaqe3gqrtbmekhxqglvzau73ad.onion/">uzmim34vid5dkyzjxpel6zbkfz4qeqaaqe3gqrtbmekhxqglvzau73ad.onion</a>
+      <a href="http://uzmim34vid5dkyzjxpel6zbkfz4qeqaaqe3gqrtbmekhxqglvzau73ad.onion/">uzmim34vid5dkyzjxpel6zbkfz4qeqaaqe3gqrtbmekhxqglvzau73ad.onion</a>
     </v-card-text>
   </v-card>
 </template>
