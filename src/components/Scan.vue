c586eed340859ed0ca76347e0e82be18cee9e80c
Adam Soltys <asoltys@gmail.com>
2020-05-13 20:25:13 -0700

    new scanner again

--- src/components/Scan.vue
+++ src/components/Scan.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    HI
+    <video v-show="false" ref="video" />
     <div class="d-flex justify-center">
       <v-progress-linear v-if="loading" indeterminate />
-      <qrcode-stream class="mt-4" @decode="handleScan" />
+      <canvas class="flex-grow-1" v-else ref="canvas" />
     </div>
     <div class="d-flex">
       <v-btn @click="stop" class="mr-2 my-2 flex-grow-1">
@@ -15,21 +15,79 @@
 </template>
 
 <script>
+import jsQR from 'jsqr';
 import { call } from 'vuex-pathify';
-import { QrcodeStream } from 'vue-qrcode-reader';
 
 export default {
   data() {
     return {
       loading: false,
-    } 
+    };
   },
-  components: { QrcodeStream },
+
   methods: {
-    handleScan: call('handleScan'),
     stop() {
-      this.$router.go(-1);
-    } 
+      this.$refs.video.srcObject.getTracks().map(t => t.stop());
+      this.handleScan();
+    },
+    handleScan: call('handleScan'),
+
+    tick() {
+      let { video } = this.$refs;
+      if (!video) return;
+      if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        this.loading = false;
+
+        this.$nextTick(() => {
+          var canvasElement = this.$refs.canvas;
+          var canvas = canvasElement.getContext('2d');
+
+          canvasElement.height = video.videoHeight;
+          canvasElement.width = video.videoWidth;
+
+          canvas.drawImage(
+            video,
+            0,
+            0,
+            canvasElement.width,
+            canvasElement.height
+          );
+          var imageData = canvas.getImageData(
+            0,
+            0,
+            canvasElement.width,
+            canvasElement.height
+          );
+
+          var code = jsQR(imageData.data, imageData.width, imageData.height, {
+            inversionAttempts: 'dontInvert',
+          });
+
+          if (code) this.handleScan(code.data);
+        });
+      }
+
+      requestAnimationFrame(this.tick);
+    },
+  },
+
+  mounted() {
+    this.loading = true;
+    let { tick } = this;
+    let video = this.$refs.video;
+
+    navigator.mediaDevices
+      .getUserMedia({ video: { facingMode: 'environment' } })
+      .then(function(stream) {
+        video.srcObject = stream;
+        video.setAttribute('playsinline', true); // required to tell iOS safari we don't want fullscreen
+        video.play();
+        requestAnimationFrame(tick);
+      });
+  },
+
+  beforeDestroy() {
+    this.$refs.video.srcObject.getTracks().map(t => t.stop());
   } 
 };
 </script>
