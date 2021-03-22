fd0845707556e9e4d1429d2f27846e0824dd78e7
Adam Soltys <asoltys@gmail.com>
2021-03-21 22:08:22 -0700

    registration form cleanup

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,31 +1,40 @@
 <template>
-  <div>
-    <v-progress-linear v-if="loading" indeterminate />
-    <v-form v-else @submit.prevent="submit" class="mt-4">
-      <h2>New Account</h2>
-      <v-text-field
-        label="Username"
-        v-model="form.username"
-        dark
-        autocapitalize="none"
-        ref="username"
-      />
-      <v-text-field
-        label="Password"
-        v-model="form.password"
-        type="password"
-        ref="password"
-      />
-      <v-btn type="submit" color="green" class="wide mr-2">
-        <v-icon left>$wallet</v-icon>
-        Create New Account
-      </v-btn>
-      <v-btn @click="$go('/')" class="mb-1 mb-sm-0 wide">
-        <v-icon left>$left</v-icon>
-        Back
-      </v-btn>
-    </v-form>
-  </div>
+  <v-card>
+    <v-card-text>
+      <v-progress-linear v-if="loading" indeterminate />
+      <v-form v-else @submit.prevent="submit" class="mt-4">
+        <h2 class="mb-4 white--text">Register an Account</h2>
+        <v-text-field
+          label="Username"
+          v-model="form.username"
+          dark
+          autocapitalize="none"
+          ref="username"
+        />
+        <v-text-field
+          label="Password"
+          v-model="form.password"
+          type="password"
+          ref="password"
+        />
+        <v-btn-toggle
+          tile
+          color="primary accent-3"
+          group
+          class="d-flex flex-wrap mx-auto"
+        >
+          <v-btn type="submit" class="wide mr-2 flex-grow-1">
+            <v-icon left color="green">$forward</v-icon>
+            Register
+          </v-btn>
+          <v-btn @click="$go('/')" class="mb-1 mb-sm-0 wide flex-grow-1">
+            <v-icon left color="red">$cancel</v-icon>
+            Nevermind
+          </v-btn>
+        </v-btn-toggle>
+      </v-form>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
