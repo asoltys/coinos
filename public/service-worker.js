0ca86a8c163a4cf52c1a4cf06e8322711b135e0b
Adam Soltys <asoltys@gmail.com>
2021-03-26 12:20:14 -0700

    remove fetch from sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,14 +6,6 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
-
 self.addEventListener('push', function(e) {
   const payload = e.data ? e.data.text() : '';
   var options = {
