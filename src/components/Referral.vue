b7c8370e43ff556178b4493ab563b52243b22212
Ran Guin <rguin@iMac.local>
2021-05-15 22:38:34 -0700

    tweak referral call

--- src/components/Referral.vue
+++ src/components/Referral.vue
@@ -5,28 +5,17 @@ div
     v-card-text
       p Note: each referral token can only be used by one person
     v-card-actions
-      v-btn(@click='generateReferral()', color='green') Generate Referral Token
-      v-btn(@click='checkTokens()', color='green') Check Available Tokens
+      v-row.justify-space-around
+        v-btn(@click='generateReferral()', color='green') Generate Referral Token
+        v-btn(@click='checkTokens()', color='green') List Referral Tokens
   hr
   v-card
     v-card-title Referral Token(s)
     v-card-text
       div(v-if='tokens && tokens.length')
-        ol
-          li(v-for='token in tokens', v-if='(token.status = "pending")')
-            b {{ token.token }} &nbsp; &nbsp;
-            span(v-if='token.expiry') (expires: {{ token.expiry }})
+        v-data-table(:items='tokens' :headers='tokenHeaders')
       div(v-else) No Tokens Found
       hr
-      div(v-if='showTokens')
-        v-btn(@click='showUsed = true', color='green', small) Show Used Tokens
-        div(v-if='showUsed')
-          p Unavailable tokens:
-          ol
-            li(v-for='token in tokens', v-if='token.status != "pending"')
-              b {{ token.token }} [{{ token.status }}]
-              span(v-if='token.expiry') (expiration: {{ token.expiry }})
-        v-btn(@click='showUsed = true')
       p(v-if='message') &nbsp;
         b.message() {{ message }}
       p(v-if='warning')
@@ -49,26 +38,47 @@ export default {
       message: '',
       warning: '',
       showUsed: false,
+
+      newTokenHeaders: [
+        {text: 'token', value: 'token'},
+        {text: 'status', value: 'status'},
+      ],
+      allTokenHeaders: [
+        {text: 'token', value: 'token'},
+        {text: 'status', value: 'status'},
+        {text: 'created_at', value: 'created_at'},
+        {text: 'used_by', value: 'used_by'},
+      ],
+      tokenHeaders: []
     };
   },
   computed: {
     user: get('user'),
   },
   methods: {
+    resetMessages () {
+      this.warning = ''
+      this.message = ''
+    },
     generateReferral () {
       const options = {
         sponsor_id: this.user.id,
         expiry: null,
       };
 
+      this.tokens = []
+      this.tokenHeaders = this.newTokenHeaders
+
+      this.resetMessages()
       Vue.axios
-        .post('/referrals/grant', options)
+        // .post('/referrals/grant', options)
+        .get('/referrals/grant?sponsor_id=' + this.user.id)
         .then((response) => {
           console.log("Response: " + JSON.stringify(response))
-          if (response.token) {
-            const token = response.token;
+          if (response.data) {
+            const token = response.data;
             this.tokens.push(token);
-            this.warning = 'Referral token generated...';
+            this.message = 'Referral token generated...';
           } else {
             this.warning = 'Error accessing referral token';
           }
@@ -82,14 +92,23 @@ export default {
     checkTokens() {
       this.showTokens;
       this.tokens = [];
-      this.message = '';
-      this.warning = '';
+      this.tokenHeaders = this.allTokenHeaders
+
+      this.resetMessages()
+
       Vue.axios
         .get('/referrals/checkTokens/' + this.user.id)
         .then((response) => {
-          if (response) {
-            console.log("Response: " + JSON.stringify(response))
-            this.tokens = response.tokens || [];
+          if (response && response.data) {
+            console.log("Response: " + JSON.stringify(response.data))
+            var tokens = response.data.tokens || [];
+            for (var i = 0; i < tokens.length; i++) {
+              console.log(i + ' / ' + tokens.length)
+              if (tokens[i].user) {
+                this.$set(tokens[i], 'used_by', tokens[i].user.username)
+              }
+            }
+            this.tokens = tokens
           } else {
             this.warning = 'No active referral tokens';
           }
