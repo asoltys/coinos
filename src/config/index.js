5aed63e49feeed681b577bf2ec5fb0f2a3f352e2
Ran Guin <rguin@iMac.local>
2021-05-20 21:11:03 -0700

    add client side config (include rules & province references for now)

---
+++ src/config/index.js
@@ -0,0 +1,71 @@
+
+module.exports = {
+  provinces: [
+    {
+      text: 'Alberta',
+      value: 'AB',
+    },
+    {
+      text: 'British Columbia',
+      value: 'BC',
+    },
+    {
+      text: 'Manitoba',
+      value: 'MB',
+    },
+    {
+      text: 'New Brunswick',
+      value: 'NB',
+    },
+    {
+      text: 'Newfoundland and Labrador',
+      value: 'NL',
+    },
+    {
+      text: 'Northwest Territories',
+      value: 'NT',
+    },
+    {
+      text: 'Nova Scotia',
+      value: 'NS',
+    },
+    {
+      text: 'Nunavut',
+      value: 'NU',
+    },
+    {
+      text: 'Ontario',
+      value: 'ON',
+    },
+    {
+      text: 'Prince Edward Island',
+      value: 'PE',
+    },
+    {
+      text: 'Quebec',
+      value: 'QC',
+    },
+    {
+      text: 'Saskatchewan',
+      value: 'SK',
+    },
+    {
+      text: 'Yukon Territory',
+      value: 'YT',
+    }
+  ],
+
+  rules: {
+    email: [
+      v => !!v || 'Email is required',
+      v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+    ],
+    formattedPhone: [
+      v => !v || /^\+1 \(\d\d\d\) \d\d\d\-\d\d\d\d$/.test(v) || 'Phone must be valid and include area code'
+    ],
+    phone: [
+      v => !v || /^\(\d\d\d\) \d\d\d\-\d\d\d\d$/.test(v) || 'phone must be valid - eg (604) 123-4567'
+    ],
+
+  }
+}
