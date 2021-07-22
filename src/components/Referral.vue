659542ce1665f2195f4be0198c8e99e4858fffc2
Ran Guin <rguin@iMac.local>
2021-07-22 15:40:27 -0700

    await user info and only enable referral options for referred users

--- src/components/Referral.vue
+++ src/components/Referral.vue
@@ -1,37 +1,41 @@
 <template lang="pug">
 div
-  v-card
-    v-card-title Generate Referral Token(s) for Friends
-    v-card-text
-      p Each referral token can only be used by one person (send them the token string).
-      p They will not need the token to register, but will need it to access funding options. 
-      //- p Once this user has opened an account you may be eligible for referral rewards. 
-    v-card-actions
-      v-row.justify-space-around
-        v-btn(@click='generateReferral()', color='green') Generate New Token
-        v-btn(@click='checkTokens("available")', color='green') Available
-        v-btn(@click='checkTokens("used")', color='green') Used
-  hr
-  v-card
-    v-card-title {{status}} Referral Token(s)
-    v-card-text
-      div(v-if='tokens && tokens.length')
-        //- v-list
-        //-   v-list-item(v-for='token in tokens')
-        //-     v-list-item-title Token
-        //-     v-list-item-content {{token.token}}
-        //-     v-list-item-title Created
-        //-     v-list-item-content {{token.created_at.substring(0,10)}}
-        //-     v-list-item-title Used By
-        //-     v-list-item-content {{token.user}}
+  div(v-if='isReferred')
+    v-card
+      v-card-title Generate Referral Token(s) for Friends
+      v-card-text
+        p Each referral token can only be used by one person (send them the token string).
+        p They will not need the token to register, but will need it to access funding options. 
+        //- p Once this user has opened an account you may be eligible for referral rewards. 
+      v-card-actions
+        v-row.justify-space-around
+          v-btn(@click='generateReferral()', color='green') Generate New Token
+          v-btn(@click='checkTokens("available")', color='green') Available
+          v-btn(@click='checkTokens("used")', color='green') Used
+    hr
+    v-card
+      v-card-title {{status}} Referral Token(s)
+      v-card-text
+        div(v-if='tokens && tokens.length')
+          //- v-list
+          //-   v-list-item(v-for='token in tokens')
+          //-     v-list-item-title Token
+          //-     v-list-item-content {{token.token}}
+          //-     v-list-item-title Created
+          //-     v-list-item-content {{token.created_at.substring(0,10)}}
+          //-     v-list-item-title Used By
+          //-     v-list-item-content {{token.user}}
 
-        v-data-table(:items='tokens' :headers='tokenHeaders')
-      div(v-else) No Tokens Found
-      hr
-      p(v-if='message') &nbsp;
-        b.message() {{ message }}
-      p(v-if='warning')
-        b.warning {{ warning }}
+          v-data-table(:items='tokens' :headers='tokenHeaders')
+        div(v-else) No Tokens Found
+        hr
+        p(v-if='message') &nbsp;
+          b.message() {{ message }}
+        p(v-if='warning')
+          b.warning {{ warning }}
+  v-progress-linear(v-else-if='loading' indeterminate='')
+  div(v-else)
+    v-alert(color='red') Only referred members can refer other friends
 
 
 </template>
@@ -41,8 +45,13 @@ import { call, get } from 'vuex-pathify';
 import axios from 'axios';
 import Vue from 'vue';
 
+import DynamicLoad from '@/mixins/DynamicLoad'
+
 export default {
   props: {},
+  mixins: [
+    DynamicLoad
+  ],
   data() {
     return {
       tokens: [],
@@ -62,11 +71,29 @@ export default {
         {text: 'created', value: 'created'},
         {text: 'used_by', value: 'used_by'},
       ],
-      tokenHeaders: []
+      tokenHeaders: [],
+      isReferred: false,
+      loading: true
     };
   },
   computed: {
     user: get('user'),
+  },
+  async mounted () {
+    await this.waitForUser(5)
+
+    console.log('check referral status for ' + this.user.id)
+    Vue.axios.get('/referrals/isReferred/' + this.user.id)
+      .then( response => {
+        this.isReferred = response.data.referred
+        console.log('referred: ' + this.isReferred)
+        this.loading = false
+      })
+      .catch( err => {
+        this.isReferred = null
+        console.debug('error checking referral')
+        this.loading = false
+      })
   },
   methods: {
     resetMessages () {
