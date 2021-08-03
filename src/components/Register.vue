323b5b1fad4248b331e5e89c5479873714da4663
Adam Soltys <asoltys@gmail.com>
2021-08-03 01:29:48 -0700

    restore old registration page

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,26 +1,45 @@
-<template lang='pug'>
-  v-card
-    v-card-text
-      v-progress-linear(v-if='loading' indeterminate='')
-      v-form.mt-4(v-model='validForm' v-else='' @submit.prevent="register()")
-        h2.mb-4.white--text Register an Account
-        v-text-field.validate(label='Username' v-model='form.username' dark='' autocapitalize='none' ref='username' autocomplete='username' :rules='rules.username' append-icon='$account')
-        v-text-field.validate(label='Email' type='email' v-model='form.email' dark='' autocapitalize='none' ref='email' autocomplete='email'  append-icon='$email' :rules='rules.email')
-        v-text-field(label='Phone (optional)' @change='validatePhone()' v-model='form.phone' dark='' autocapitalize='none' ref='phone' append-icon='$cellphone' autocomplete='phone' :rules='rules.NAPhone' :class='phoneValidated ? "validated" : "unvalidated"')
-        v-text-field.validate(label='Password' v-model='form.password' type='password' ref='password' autocomplete='current-password' :rules='rules.password' append-icon='$lock')
-        v-btn-toggle.d-flex.flex-wrap.mx-auto(tile='' color='primary accent-3' group='')
-          v-btn.wide.mr-2.flex-grow-1(:disabled='!validForm' type='submit')
-            v-icon(left='' color='green') $forward
-            span Register
-          v-btn.mb-1.mb-sm-0.wide.flex-grow-1(@click="$go('/')")
-            v-icon(left='' color='red') $cancel
-            span Nevermind
+<template>
+  <v-card>
+    <v-card-text>
+      <v-progress-linear v-if="loading" indeterminate />
+      <v-form v-else @submit.prevent="submit" class="mt-4">
+        <h2 class="mb-4 white--text">Register an Account</h2>
+        <v-text-field
+          label="Username"
+          v-model="form.username"
+          dark
+          autocapitalize="none"
+          ref="username"
+        />
+        <v-text-field
+          label="Password"
+          v-model="form.password"
+          type="password"
+          ref="password"
+        />
+        <v-btn-toggle
+          tile
+          color="primary accent-3"
+          group
+          class="d-flex flex-wrap mx-auto"
+        >
+          <v-btn type="submit" class="wide mr-2 flex-grow-1">
+            <v-icon left color="green">$forward</v-icon>
+            Register
+          </v-btn>
+          <v-btn @click="$go('/')" class="mb-1 mb-sm-0 wide flex-grow-1">
+            <v-icon left color="red">$cancel</v-icon>
+            Nevermind
+          </v-btn>
+        </v-btn-toggle>
+      </v-form>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
 import { call, get, sync } from 'vuex-pathify';
 const IS_PRODUCTION = !['development', 'test'].includes(process.env.NODE_ENV);
-import config from '@/config'
 
 export default {
   props: {
@@ -29,19 +48,12 @@ export default {
   },
   data() {
     return {
-      validForm: false,
       submitted: false,
       form: {
         username: this.username,
-        email: this.email,
-        phone: this.phone,
         password: this.password,
         response: '',
-        message: ''
       },
-      phoneIcon: '$cancel',
-      phoneValidated: false,
-      rules: {}
     };
   },
 
@@ -53,68 +65,16 @@ export default {
   methods: {
     createUser: call('createUser'),
     login: call('login'),
-    async register() {
+    async submit() {
       let user = await this.createUser(this.form);
       user.password = this.form.password;
-      console.log("User created...")
       await this.login(user);
-      console.log("User logged in...")
-      this.$go('/home')
     },
-    validatePhone () {
-      this.phoneValidated = false
-      this.phoneIcon = 'mdi-close'
-      var digits = this.form.phone.replace(/\D/g, '')
-      console.log(digits.length + ' digits entered: ' + this.form.phone + ' -> ' + digits)
-      var formattedPhone
-      if (digits.length === 11 && digits.substring(0,1) === '1') {
-        formattedPhone = '+1 (' + digits.substring(1,4) + ') ' + digits.substring(4, 7) + '-' + digits.substring(7,11)
-      } else if (digits.length === 10) {
-        formattedPhone = '+1 (' +  digits.substring(0,3) + ') ' + digits.substring(3, 6) + '-' + digits.substring(6,10)
-        this.$set(this.form, 'phone', formattedPhone)
-        this.phoneValidated = true
-        this.phoneIcon = 'mdi-check'
-        console.log('international format: ' + formattedPhone)
-      } else if (digits.substring(0,1) === '+') {
-        // International number
-        formattedPhone = this.form.phone
-        console.log('international format entered: ' + formattedPhone)
-      }
-
-      if (formattedPhone) {
-        console.log('re-formatted: ' + formattedPhone)
-        this.$set(this.form, 'phone', formattedPhone)
-        this.phoneValidated = true
-        this.phoneIcon = '$check'
-        this.$set(this.form, 'formattedPhone', formattedPhone)
-      } else {
-        this.phoneValidated = false
-        this.phoneIcon = '$cancel'
-      }
-    }
   },
 
   mounted() {
-
-    this.rules = config.rules || {}
-
     if (!this.username) this.$refs.username.focus();
     else if (!this.password) this.$refs.password.focus();
   },
 };
 </script>
-<style>
-.v-text-field.validated .v-input__icon--append .v-icon { 
-    color: green;
-}
-.v-text-field.unvalidated .v-input__icon--append .v-icon { 
-    color: red;
-}
-
-.v-text-field.validate .v-input__icon--append .v-icon { 
-    color: green;
-}
-.v-text-field.validate.error--text .v-input__icon--append .v-icon { 
-    color: red;
-}
-</style>
