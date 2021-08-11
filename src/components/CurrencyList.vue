9c79b166a07ee1647e2dac296f04fdbfb068d0c2
Adrien Hopkins <ahopk127@my.yorku.ca>
2021-08-11 11:58:00 -0500

    Added giga- and tera-satoshis, and sorted denominations by size

--- src/components/CurrencyList.vue
+++ src/components/CurrencyList.vue
@@ -49,7 +49,7 @@ export default {
   methods: {
     color(c) {
       if (!this.user.currencies) return 'white';
-      return ['BTC', 'SAT', 'KSAT', 'MSAT'].includes(c)
+      return ['SAT', 'KSAT', 'MSAT', 'BTC', 'GSAT', 'TSAT'].includes(c)
         ? 'white'
         : this.type === "accounts"
         ? 'liquid'
@@ -85,7 +85,7 @@ export default {
 
       if (!currency && this.user.fiat) await this.toggleFiat();
 
-      if (['BTC', 'SAT', 'KSAT', 'MSAT'].includes(c))
+      if (['SAT', 'KSAT', 'MSAT', 'BTC', 'GSAT', 'TSAT'].includes(c))
         return this.setUnit(c);
     },
   },
