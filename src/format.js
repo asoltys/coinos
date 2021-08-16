26d9bd3ca9f37cb49d758ce62c607b92db7b7b23
Adrien Hopkins <ahopk127@my.yorku.ca>
2021-08-16 18:25:45 -0500

    Fixed: using TSAT breaks transactions menu
    Also fixed the issue with large denominations & small values for real

--- src/format.js
+++ src/format.js
@@ -3,14 +3,15 @@ export default function(n, p) {
   else {
     let x = n / 10 ** p;
     if (Math.abs(x) < 1.0) {
-      var e = parseInt(x.toString().split('e-')[1]);
+      let e = parseInt(x.toString().split('e-')[1]);
       if (e) {
-        x *= Math.pow(10, e - 1);
-        x = x.toFixed(9 - e);
-        x = '0.' + new Array(e).join('0') + x.toString().substring(2);
+        x *= Math.pow(10, e);
+        if (x.toString().indexOf('.') > -1)
+          x = x.toString().replace('.', '');
+        x = '0.' + new Array(e).join('0') + x;
       }
     } else {
-      var e = parseInt(x.toString().split('+')[1]);
+      let e = parseInt(x.toString().split('+')[1]);
       if (e > 20) {
         e -= 20;
         x /= Math.pow(10, e);
