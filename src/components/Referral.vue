e5620c465de9baeb267775625e40ebc01a420516
Adam Soltys <asoltys@gmail.com>
2021-08-19 15:36:10 -0700

    prettier

--- src/components/Referral.vue
+++ src/components/Referral.vue
@@ -6,7 +6,7 @@ div
         v-card-title Generate Referral Token(s) for Friends
         v-card-text
           p Each referral token can only be used by one person (send them the token string).
-          p They will not need the token to register, but will need it to access funding options. 
+          p They will not need the token to register, but will need it to access funding options.
           //- p Once this user has opened an account you may be eligible for referral rewards. 
         v-card-actions
           v-row.justify-space-around
@@ -17,7 +17,7 @@ div
             v-btn(@click='checkTokens("used")', color='green') Used
       hr
       v-card
-        v-card-title {{status}} Referral Token(s)
+        v-card-title {{ status }} Referral Token(s)
         v-card-text
           div(v-if='tokens && tokens.length')
             //- v-list
@@ -28,11 +28,11 @@ div
             //-     v-list-item-content {{token.created_at.substring(0,10)}}
             //-     v-list-item-title Used By
             //-     v-list-item-content {{token.user}}
-            v-data-table(:items='tokens' :headers='tokenHeaders')
+            v-data-table(:items='tokens', :headers='tokenHeaders')
           div(v-else) No Tokens Found
           hr
           p(v-if='message') &nbsp;
-            b.message() {{ message }}
+            b.message {{ message }}
           p(v-if='warning')
             b.warning {{ warning }}
     div(v-show='openQR')
@@ -40,39 +40,40 @@ div
         v-card-title
           b.text-center New QR Referral Link
         v-card-text
-          v-alert(color='yellow' light) Any logged in user can scan this barcode to become referred
+          v-alert(color='yellow', light) Any logged in user can scan this barcode to become referred
           v-row.justify-space-around
             canvas#qrCanvas
         v-card-actions
           v-row.justify-space-around
-            v-btn(@click='openQR=false' color='green')
+            v-btn(@click='openQR = false', color='green')
               v-icon(large) $cancel
-  v-progress-linear(v-else-if='loading' indeterminate='')
+  v-progress-linear(v-else-if='loading', indeterminate='')
   div(v-else)
     v-alert(color='red') Only referred members can refer other friends
-  v-dialog(v-model='referral_link' dark max-width='500')
-      v-card(style='background-color: #333')
-        v-card-title
-          b.text-center Live Referral Link
-        v-card-text
-          v-alert(color='yellow' light) Send this link to someone you know to refer them
-          
-          v-text-field(v-model='target' placeholder='Email Address')
-          v-btn(@click='copyToClipboard("abc")') Copy
-          p(v-if='target')
-            a(:href='"mailto:" + target + "?subject=CoinOS Referral&body=<a href=" + referral_link + "> Click here to activate your referral </a>"') Link
-        v-card-actions
-          v-btn(@click='referral_link=null' color='green')
-            v-icon(large) $cancel
+  v-dialog(v-model='referral_link', dark, max-width='500')
+    v-card(style='background-color: #333')
+      v-card-title
+        b.text-center Live Referral Link
+      v-card-text
+        v-alert(color='yellow', light) Send this link to someone you know to refer them
 
+        v-text-field(v-model='target', placeholder='Email Address')
+        v-btn(@click='copyToClipboard("abc")') Copy
+        p(v-if='target')
+          a(
+            :href='"mailto:" + target + "?subject=CoinOS Referral&body=<a href=" + referral_link + "> Click here to activate your referral </a>"'
+          ) Link
+      v-card-actions
+        v-btn(@click='referral_link = null', color='green')
+          v-icon(large) $cancel
 </template>
 
 <script>
 import { call, get } from 'vuex-pathify';
 import axios from 'axios';
 import Vue from 'vue';
-import QRCode from 'qrcode'
-import config from '@/config'
+import QRCode from 'qrcode';
+import config from '@/config';
 
 import DynamicLoad from '@/mixins/DynamicLoad';
 
@@ -96,17 +97,17 @@ export default {
         { text: 'status', value: 'status' },
       ],
       allTokenHeaders: [
-        {text: 'token', value: 'token'},
-        {text: 'status', value: 'status'},
-        {text: 'created', value: 'created'},
-        {text: 'used_by', value: 'used_by'},
+        { text: 'token', value: 'token' },
+        { text: 'status', value: 'status' },
+        { text: 'created', value: 'created' },
+        { text: 'used_by', value: 'used_by' },
         // {text: 'link', value: 'link'}
         // {text: 'qrcode', value: 'qrcode'}
       ],
       tokenHeaders: [],
       isReferred: false,
       loading: true,
-      openQR: false
+      openQR: false,
     };
   },
   computed: {
@@ -146,62 +147,67 @@ export default {
       this.tokens = [];
       this.tokenHeaders = this.newTokenHeaders;
 
-      this.resetMessages()
-      return Vue.axios
-        // .post('/referrals/grant', options)
-        .get('/referrals/grant?sponsor_id=' + this.user.id)
-        .then((response) => {
-          // console.log("Response: " + JSON.stringify(response))
-          if (response.data) {
-            const token = response.data;
-            this.tokens.push(token);
-            this.message = 'Referral token generated...';
-            this.status = 'New';
-          } else {
-            this.warning = 'Error accessing referral token';
-          }
-        })
-        .catch((err) => {
-          var now = new Date().toISOString();
-          this.warning = 'Error requesting referral token';
-        });
+      this.resetMessages();
+      return (
+        Vue.axios
+          // .post('/referrals/grant', options)
+          .get('/referrals/grant?sponsor_id=' + this.user.id)
+          .then((response) => {
+            // console.log("Response: " + JSON.stringify(response))
+            if (response.data) {
+              const token = response.data;
+              this.tokens.push(token);
+              this.message = 'Referral token generated...';
+              this.status = 'New';
+            } else {
+              this.warning = 'Error accessing referral token';
+            }
+          })
+          .catch((err) => {
+            var now = new Date().toISOString();
+            this.warning = 'Error requesting referral token';
+          })
+      );
     },
-    async cancel () {
-      
+    async cancel() {},
+    async generateLink() {
+      await this.generateReferral();
+      var token =
+        this.status === 'New' ? this.tokens[this.tokens.length - 1].token : '';
+
+      console.log('Retrieved ' + token);
+      this.referral_link =
+        config.servers[process.env.NODE_ENV] ||
+        'https://coinos.io/funding/referral/' + token;
+      // send this link in an email message ...
     },
-    async generateLink () {
-      await this.generateReferral()
-      var token = this.status === 'New' ? this.tokens[this.tokens.length-1].token : ''
+    async generateQR(version) {
+      await this.generateReferral();
+      var token =
+        this.status === 'New' ? this.tokens[this.tokens.length - 1].token : '';
 
-      console.log('Retrieved ' + token)
-      this.referral_link = config.servers[process.env.NODE_ENV] || 'https://coinos.io/funding/referral/' + token
-      // send this link in an email message ... 
-    },
-    async generateQR (version) {
-      await this.generateReferral()
-      var token = this.status === 'New' ? this.tokens[this.tokens.length-1].token : ''
-      
-      console.log('Retrieved ' + JSON.stringify(token))
+      console.log('Retrieved ' + JSON.stringify(token));
 
-      if (!version) { version = 8 } // sizes range from 1 - 40 indicating size / capacity
-      console.log('generate QR..')
-      
-      var qr = this.referral_url + token
+      if (!version) {
+        version = 8;
+      } // sizes range from 1 - 40 indicating size / capacity
+      console.log('generate QR..');
 
-      console.log('link to: ' + JSON.stringify(qr))
+      var qr = this.referral_url + token;
 
-      var canvas = document.getElementById('qrCanvas')
+      console.log('link to: ' + JSON.stringify(qr));
+
+      var canvas = document.getElementById('qrCanvas');
       if (canvas) {
         QRCode.toCanvas(canvas, qr, { version: version }, function (error) {
-          if (error) console.error(error)
+          if (error) console.error(error);
           console.log('success!');
-        })
-        this.openQR = true
-
+        });
+        this.openQR = true;
       } else {
-        console.log('qr canvas not available')
+        console.log('qr canvas not available');
       }
-      this.checkTokens('available')
+      this.checkTokens('available');
     },
     checkTokens(status) {
       this.showTokens;
@@ -231,7 +237,7 @@ export default {
         .catch((err) => {
           this.warning = 'Error retrieving referral tokens';
         });
-    }
+    },
   },
   watch: {
     user() {
