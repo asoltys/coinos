cc05d280c0108b9a1ed065133f67efea0bd727b2
Adrien Hopkins <ahopk127@my.yorku.ca>
2021-08-31 13:20:15 -0600

    Changed text of info box
    The functionality for the new unit denominations changed in master; this commit updates the info box to reflect these changes.  I don't think these changes are currently in effect in dev.

--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -161,12 +161,9 @@ export default {
       alert(`This button allows you to change your bitcoin denomination:
 The satoshi (SAT) is the smallest denomination of bitcoin.
 It is like cents, only there are 100 million SATs to a bitcoin, not 100.
-We also support combining the SAT with standard metric prefixes:
+We also support combining the SAT with a few standard metric prefixes:
 1 KSAT (kilo-satoshi) = 1000 SAT = 0.000 01 BTC
-1 MSAT (mega-satoshi) = 1 million SAT = 0.01 BTC
-1 GSAT (giga-satoshi) = 1 billion SAT = 10 BTC
-1 TSAT (tera-satoshi) = 1 trillion SAT = 10 000 BTC
-Below you will see how much each of these is worth in your local currency.`);
+1 MSAT (mega-satoshi) = 1 million SAT = 0.01 BTC`);
     },
     toggleCustodial() {
       let a = this.user.accounts.filter((a) => a.asset === BTC);
