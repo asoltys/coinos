e14736d680028af0f523ea24b2b218c182da7e78
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-06-21 12:15:40 -0600

    hook up form to email server

--- src/components/Support.vue
+++ src/components/Support.vue
@@ -7,30 +7,28 @@
         indeterminate
         class="my-2"
       ></v-progress-linear>
-      <div v-if="!submitted">
+      <div v-if="!submitted && !loading">
         <div class="text-justify my-2">
           Please contact coinos with any questions you may have and we will get
           back to you as soon as possible.
         </div>
-        <v-text-field label="Email" v-model="email" />
-        <v-text-field label="Account Name (optional)" v-model="account" />
-        <v-textarea label="Message" v-model="message" />
-        <div class="d-flex">
-          <v-btn
-            class="flex-grow-1 wide"
-            dark
-            @click="
-              loading = true;
-              submitted = true;
-              loading = false;
-            "
-            :disabled="!message || !email || submitted"
-          >
-            <v-icon left color="green">$question</v-icon><span>Submit</span>
-          </v-btn>
-        </div>
+        <v-form @submit.prevent="submit">
+          <v-text-field label="Email" v-model="email" />
+          <v-text-field label="Account Name (optional)" v-model="account" />
+          <v-textarea label="Message" v-model="message" />
+          <div class="d-flex">
+            <v-btn
+              class="flex-grow-1 wide"
+              dark
+              type="submit"
+              :disabled="!message || !email || submitted"
+            >
+              <v-icon left color="green">$question</v-icon><span>Submit</span>
+            </v-btn>
+          </div>
+        </v-form>
       </div>
-      <div v-if="submitted">
+      <div v-if="submitted && !loading">
         <div class="text-justify my-2">
           Thanks for contacting us! Please check your e-mail inbox for a
           response within the next few days.
@@ -38,8 +36,9 @@
         <div class="d-flex">
           <v-btn
             @click="
-              message = null;
               email = null;
+              account = null;
+              message = null;
               submitted = false;
             "
             class="my-2 flex-grow-1 wide"
@@ -54,6 +53,7 @@
 </template>
 
 <script>
+import { call } from 'vuex-pathify';
 export default {
   data: () => ({
     email: null,
@@ -62,5 +62,17 @@ export default {
     loading: false,
     submitted: false,
   }),
+  methods: {
+    async submit() {
+      this.loading = true;
+      this.submitted = await this.submitSupport({
+        email: this.email,
+        account: this.account,
+        message: this.message,
+      });
+      this.loading = false;
+    },
+    submitSupport: call('submitSupport'),
+  },
 };
 </script>
